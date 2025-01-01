2f49762b2961ad8d6b24ad9d73bf556188b0c183
omtripathi786 <omtripathi88@gmail.com>
2025-01-01 20:39:47 +0530

    some leet code questions added.

---
+++ LEET_CODE_TOP_100/Arrays_ Strings/longest_common_prefix.py
@@ -0,0 +1,37 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+"""
+
+def solution(strs):
+    #TIME: O(n*m)
+    #SPACE: O(1)
+    min_length = len(min(strs, key=len))
+    i = 0
+    while i < min_length:
+        for s in strs:
+            if s[i] != strs[0][i]:
+                return s[:i]
+        i += 1
+    return s[:i]
+
+if __name__ == '__main__':
+    strs = ["flower","flow","flight"]
+    print(solution(strs))
