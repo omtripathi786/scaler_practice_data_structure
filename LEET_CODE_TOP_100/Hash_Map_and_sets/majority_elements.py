ed08a4175ea350c54e6b87d4f2d5885475617e5a
omtripathi786 <omtripathi88@gmail.com>
2025-01-16 16:44:34 +0530

    hash map and set questions added.

---
+++ LEET_CODE_TOP_100/Hash_Map_and_sets/majority_elements.py
@@ -0,0 +1,54 @@
+"""
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+"""
+
+
+def brute_force(nums):
+    counter = {}
+    for num in nums:
+        if num not in counter:
+            counter[num] = 1
+        else:
+            counter[num] += 1
+
+    max_count = float('-inf')
+    ans = float('-inf')
+    for key, value in counter.items():
+        if value > max_count:
+            max_count = value
+            ans = key
+    return ans
+
+
+def solution(nums):
+    count = 0
+    ans = float('-inf')
+    for num in nums:
+        if count == 0:
+            ans = num
+        if ans == num:
+            count += 1
+        else:
+            count -= 1
+    return ans
+
+
+if __name__ == '__main__':
+    nums = [2, 2, 1, 1, 1, 2, 2]
+    print(brute_force(nums))
+    print(solution(nums))
---
+++ LEET_CODE_TOP_100/Hash_Map_and_sets/validate_sudoku.py
@@ -0,0 +1,86 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+"""
+
+
+def solution(board):
+    # validate row
+    for i in range(9):
+        s = set()
+        for j in range(9):
+            item = board[i][j]
+            if item in s:
+                return False
+            elif item != ".":
+                s.add(item)
+
+    # validate col
+    for i in range(9):
+        s = set()
+        for j in range(9):
+            item = board[j][i]
+            if item in s:
+                return False
+            elif item != ".":
+                s.add(item)
+
+    # validate Boxes
+    starts = [(0, 0), (0, 3), (0, 6),
+              (3, 0), (3, 3), (3, 6),
+              (6, 0), (6, 3), (6, 6)]
+
+    for i, j in starts:
+        s = set()
+        for row in range(i, i + 3):
+            for col in range(j, j + 3):
+                item = board[row][col]
+                if item in s:
+                    return False
+                elif item != ".":
+                    s.add(item)
+    return True
+
+
+if __name__ == '__main__':
+    board = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+        , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+        , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+        , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+        , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+        , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+        , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+        , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+        , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+    print(solution(board))
